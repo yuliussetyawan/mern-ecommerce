a10a288b8adf208fb663a77e67ae0ca577cedcf1
yuliussetyawan <yuliusetyawan@gmail.com>
2024-03-26 13:46:59 +0700

    Rating Component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
@@ -11,10 +12,18 @@ const Product = ({ product }) => {
 
       <Card.Body>
         <Link href={`/product/${product._id}`}>
-          <Card.Title as="div">
+          {/* text-ellipsis ... */}
+          <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
 
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
