50622f7ab814f3e118bcd7a07d8f4926e2975ea1
yuliussetyawan <yuliusetyawan@gmail.com>
2024-03-26 12:41:50 +0700

    Implement react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 const App = () => {
   return (
     <>
-      <Header/>
-      <main className='py-3'>
+      <Header />
+      <main className="py-3">
         <Container>
-          <HomeScreen/>
+          <Outlet />
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouteProvider,
+  RouterProvider,
+} from "react-router-dom";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import HomeScreen from "./screens/HomeScreen";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/* if we're on just slash and that's the index then show the homescreen */}
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
