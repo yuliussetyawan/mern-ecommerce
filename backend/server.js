54128d486ba0ebcb6c00fd6bf303f0e3d05baf59
yuliussetyawan <yuliusetyawan@gmail.com>
2024-03-28 20:54:13 +0700

    connect with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config()
 // using ES module, so need to add the file format for the import (.js)
 import products from "./data/products.js";
+import { connect } from "mongoose";
 
 const port = process.env.PORT || 5000;
+connectDB();
 const app = express();
 app.get("/", (req, res) => {
   res.send("API is running...");
