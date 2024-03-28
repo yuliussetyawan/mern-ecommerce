aa0675e2f39c582a103d08849c65f5b8394e99a3
yuliussetyawan <yuliusetyawan@gmail.com>
2024-03-28 14:59:52 +0700

    mongodb atlas setup

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.get("/api/products", (req, res) => {
   res.send(products);
 });
 
-app.get("/api/product/:id", (req, res) => {
+app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.send(product);
 });
