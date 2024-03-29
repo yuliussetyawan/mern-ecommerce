e208a0fc99e2006fd9af392233d383a077099207
yuliussetyawan <yuliusetyawan@gmail.com>
2024-03-29 13:45:41 +0700

    seeding sample data

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,27 +1,31 @@
 import mongoose from "mongoose";
 
-const reviewSchema = mongoose.Schema({
-  user: {
-    //  special type used in MongoDB to represent unique identifiers (IDs) for documents in a collection.
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    // establishes a relationship between the user field in the productSchema
-    ref: "User",
+const reviewSchema = mongoose.Schema(
+  {
+    user: {
+      //  special type used in MongoDB to represent unique identifiers (IDs) for documents in a collection.
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      // establishes a relationship between the user field in the productSchema
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  rating: {
-    type: Number,
-    required: true,
-  },
-  comment: {
-    type: String,
-    required: true,
-  },
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const productSchema = new mongoose.Schema(
   {
