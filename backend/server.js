1296bd7a2350057ae2c14d4f1cfaaa9c2d14da99
yuliussetyawan <yuliusetyawan@gmail.com>
2024-04-23 10:13:36 +0700

    user routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 // using ES module, so need to add the file format for the import (.js)
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -14,9 +15,10 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.use('/api/products',productRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
