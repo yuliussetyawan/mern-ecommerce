50e851650fc0c98b32042d770549986307925dba
yuliussetyawan <yuliusetyawan@gmail.com>
2024-04-27 08:37:02 +0700

    user email & password validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,18 @@ import User from "../models/userModel.js";
 // @route   GET /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    throw new Error("Invalid email or password");
+  }
   res.send("auth user");
 });
 
@@ -40,23 +52,22 @@ const getUsers = asyncHandler(async (req, res) => {
 // @route   DELETE /api/users/:id
 // @access  Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
-  res.send('delete user');
+  res.send("delete user");
 });
 
 // @desc    Get user by ID
 // @route   GET /api/users/:id
 // @access  Private/Admin
 const getUserById = asyncHandler(async (req, res) => {
-  res.send('get user by id');
+  res.send("get user by id");
 });
 
 // @desc    Update user
 // @route   PUT /api/users/:id
 // @access  Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-  res.send('update user');
+  res.send("update user");
 });
-
 
 export {
   authUser,
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -25,6 +26,11 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// compare password for useController
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+}
 
 const User = mongoose.model("User", userSchema);
 
