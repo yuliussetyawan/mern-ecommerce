ee3d69c5e4fba8451b9cf9d2b982f3dd31d1f71d
yuliussetyawan <yuliusetyawan@gmail.com>
2024-04-29 13:59:54 +0700

    logout user and clear cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,6 +43,17 @@ const registerUser = asyncHandler(async (req, res) => {
   res.send("register user");
 });
 
+// @desc    Logout user /  clear cookie
+// @route   POST /api/users/logout
+// @access  Private
+const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out succesfully" });
+});
+
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
