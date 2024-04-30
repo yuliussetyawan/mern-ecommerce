0cafef5250105276eac471149668c1e9cbbbd133
yuliussetyawan <yuliusetyawan@gmail.com>
2024-04-30 22:03:26 +0700

    user register endpoint & encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
-import jwt from "jsonwebtoken";
+import generateToken from "../utils/generateToken.js";
 
 // @desc    Auth user & get token
 // @route   GET /api/users/auth
@@ -9,20 +9,8 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
-    // create jwt token
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "30d",
-    });
-
-    // Set JWT as HTTP-Only
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      // if not in the development, secure set to true
-      secure: process.env.NODE_ENV !== "development",
-      sameSite: "strict",
-      // 30 days
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-    });
+    // generate jwt token
+    generateToken(res, user._id);
 
     res.json({
       _id: user._id,
@@ -40,7 +28,35 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.send("register user");
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // the password will be encrypted by userModel 'pre' property
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc    Logout user /  clear cookie
