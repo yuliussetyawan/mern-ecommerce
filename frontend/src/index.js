d057100a499ca9f49865b33ee186de2cf4ef8635
yuliussetyawan <yuliusetyawan@gmail.com>
2024-05-08 22:04:15 +0700

    user registration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import RegisterScreen from './screens/RegisterScreen';
 import store from "./store";
 import { Provider } from "react-redux";
 import LoginScreen from "./screens/LoginScreen";
@@ -26,6 +27,7 @@ const router = createBrowserRouter(
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
     </Route>
   )
 );
