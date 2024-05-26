9ce72d35559f1d9f934c40a766ce674e2c0ed344
yuliussetyawan <yuliusetyawan@gmail.com>
2024-05-26 19:59:21 +0700

    order routes & controller

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 // using ES module, so need to add the file format for the import (.js)
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -25,8 +26,10 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
