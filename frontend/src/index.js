a5115b4e51648c0dfbe930674d2caaba30bbae8e
yuliussetyawan <yuliusetyawan@gmail.com>
2024-06-08 21:35:42 +0700

    list product for admin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -27,6 +27,7 @@ import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import OrderListScreen from "./screens/admin/OrderListScreen";
+import ProductListScreen from './screens/admin/ProductListScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -42,10 +43,11 @@ const router = createBrowserRouter(
         <Route path="/payment" element={<PaymentScreen />} />
         <Route path="/placeorder" element={<PlaceOrderScreen />} />
         <Route path="/order/:id" element={<OrderScreen />} />
-        <Route path='/profile' element={<ProfileScreen />} />
+        <Route path="/profile" element={<ProfileScreen />} />
       </Route>
       <Route path="" element={<AdminRoute />}>
-        <Route path='/admin/orderlist' element={<OrderListScreen />} />
+        <Route path="/admin/orderlist" element={<OrderListScreen />} />
+        <Route path='/admin/productlist' element={<ProductListScreen />} />
       </Route>
     </Route>
   )
