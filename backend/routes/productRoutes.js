6672ea4302cf56b7bfb994e59e4610a6cdf08365
yuliussetyawan <yuliusetyawan@gmail.com>
2024-06-24 20:42:11 +0700

    Create reviews - backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,12 +4,14 @@ import {
   getProductById,
   createProduct,
   updateProduct,
-  deleteProduct
+  deleteProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 router.get("/:id", getProductById);
 router.route('/:id').get(getProductById).put(protect, admin, updateProduct);
 router
