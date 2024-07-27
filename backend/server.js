8413b5efbc6b8d5dfdf3de6a2e2d1acf6abc6130
yuliussetyawan <yuliusetyawan@gmail.com>
2024-07-27 22:15:59 +0700

    prepare for production

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,21 @@ const __dirname = path.resolve();
 // Configure to serve static files from the "/uploads" directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+if (process.env.NODE_ENV === "production") {
+  // set static folders
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  // any route that is not api will be redirected to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
+
 app.use(notFound);
 app.use(errorHandler);
 
